88f71e848bce354faa27d13113b487d9abeceb6b
Durgesh <durgeshagrawal38@gmail.com>
2024-04-13 23:52:42 +0530

    search bar added

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,7 @@
 import { Input } from "@/components/ui/input";
 import ProjectCard from "./ProjectCard";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 const PROJECTS = [
   { title: "Project1", description: "This is project 1" },
   { title: "Project2", description: "This is project 1" },
@@ -10,16 +12,24 @@ const PROJECTS = [
 ];
 const Projects = () => {
   return (
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {PROJECTS.map((project) => {
-        return (
-          <ProjectCard
-            key={project.title}
-            title={project.title}
-            description={project.description}
-          />
-        );
-      })}
+    <div className="flex flex-col justify-start align-center">
+      <div className="flex items-center w-full max-w-sm mx-auto mb-10 space-x-2">
+        <Input type="email" placeholder="Search Projects" />
+        <Button type="submit" size="icon">
+          <Search className="w-4 h-4" />
+        </Button>
+      </div>
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {PROJECTS.map((project) => {
+          return (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
