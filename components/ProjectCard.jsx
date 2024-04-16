752bfca5413090645425f0e7b44f2a3bfc86f8cb
Durgesh <durgeshagrawal38@gmail.com>
2024-04-16 23:25:26 +0530

    project details href added

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,35 +8,38 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
 
 const ProjectCard = ({ title, description, tags, age, gender }) => {
   return (
-    <Card className="transition-all duration-500 cursor-pointer hover:scale-105">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription className="text-justify">
-          {description}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        {tags.map((tag) => (
-          <Badge className="mb-2 mr-2" key={tag}>
-            {tag}
+    <Link href="/projects/1">
+      <Card className="transition-all duration-500 cursor-pointer hover:scale-105">
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription className="text-justify">
+            {description}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {tags.map((tag) => (
+            <Badge className="mb-2 mr-2" key={tag}>
+              {tag}
+            </Badge>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <h5>Age</h5>
+          <Badge
+            variant="outline"
+            className="mx-2"
+          >{`${age[0]}-${age[1]}`}</Badge>
+          <h5>Gender</h5>
+          <Badge variant="outline" className="mx-2">
+            {gender}
           </Badge>
-        ))}
-      </CardContent>
-      <CardFooter>
-        <h5>Age</h5>
-        <Badge
-          variant="outline"
-          className="mx-2"
-        >{`${age[0]}-${age[1]}`}</Badge>
-        <h5>Gender</h5>
-        <Badge variant="outline" className="mx-2">
-          {gender}
-        </Badge>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
