30e704725dfb37bbc23b32469d6de02e5d4b3226
Durgesh <durgeshagrawal38@gmail.com>
2024-04-19 01:53:08 +0530

    logout button added

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -7,7 +7,8 @@ import {
   NavigationMenuList,
   NavigationMenu,
 } from "@/components/ui/navigation-menu";
-import { ShieldPlus, Menu } from "lucide-react";
+import { ShieldPlus, Menu, LogOut } from "lucide-react";
+import authService from "../../appwrite/auth";
 
 const NAVS = [
   {
@@ -39,41 +40,54 @@ const NavItem = ({ title, url }) => {
 
 export default function Navbar() {
   return (
-    <header className="flex items-center w-full h-20 px-4 shrink-0 md:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="lg:hidden" size="icon" variant="outline">
-            <Menu className="w-6 h-6" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <Link href="/">
-            <ShieldPlus className="w-6 h-6" />
-          </Link>
-          <div className="grid gap-2 py-6">
+    <header className="flex items-center justify-between w-full h-20 px-4 shrink-0 md:px-6">
+      <div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="lg:hidden" size="icon" variant="outline">
+              <Menu className="w-6 h-6" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <Link href="/">
+              <ShieldPlus className="w-6 h-6" />
+            </Link>
+            <div className="grid gap-2 py-6">
+              {NAVS.map(({ title, url }) => (
+                <Link
+                  className="flex items-center w-full py-2 text-lg font-semibold"
+                  href={url}
+                  key={title}
+                >
+                  {title}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <Link className="hidden mr-6 lg:flex" href="/">
+          <ShieldPlus className="w-6 h-6" />
+        </Link>
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList>
             {NAVS.map(({ title, url }) => (
-              <Link
-                className="flex items-center w-full py-2 text-lg font-semibold"
-                href={url}
-                key={title}
-              >
-                {title}
-              </Link>
+              <NavItem key={title} title={title} url={url} />
             ))}
-          </div>
-        </SheetContent>
-      </Sheet>
-      <Link className="hidden mr-6 lg:flex" href="/">
-        <ShieldPlus className="w-6 h-6" />
-      </Link>
-      <NavigationMenu className="hidden lg:flex">
-        <NavigationMenuList>
-          {NAVS.map(({ title, url }) => (
-            <NavItem key={title} title={title} url={url} />
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+
+      <Button
+        onClick={async () => {
+          const loggedOut = await authService.logout();
+          console.log(loggedOut);
+        }}
+        className="float-left"
+      >
+        Logout
+        <LogOut />
+      </Button>
     </header>
   );
 }
