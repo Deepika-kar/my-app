03ac954a7b28a242218fae0e57b8ce690ce799f4
Durgesh <durgeshagrawal38@gmail.com>
2024-04-19 01:56:37 +0530

    login page fix

--- app/login/page.js
+++ app/login/page.js
@@ -43,67 +43,34 @@ function Page() {
   });
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <Tabs defaultValue="account" className="w-[400px] my-auto mx-auto">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Researcher</TabsTrigger>
-          <TabsTrigger value="password">Patient</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>Login as a Researcher.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  value={formik.values.email}
-                  onChange={(e) =>
-                    formik.setFieldValue("email", e.target.value)
-                  }
-                />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  type="password"
-                  value={formik.values.password}
-                  onChange={(e) =>
-                    formik.setFieldValue("password", e.target.value)
-                  }
-                />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button onClick={formik.handleSubmit}>Login</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>Login as a Patient.</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">UserName</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new"> password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Login</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
+      <Card className="w-[400px] my-auto mx-auto">
+        <CardHeader>
+          <CardTitle>Account</CardTitle>
+          <CardDescription>Login</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              value={formik.values.email}
+              onChange={(e) => formik.setFieldValue("email", e.target.value)}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              value={formik.values.password}
+              onChange={(e) => formik.setFieldValue("password", e.target.value)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button onClick={formik.handleSubmit}>Login</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
