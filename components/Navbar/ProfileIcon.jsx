f45992cb8a6564ab69630d00032510a3d4f5560b
Durgesh <durgeshagrawal38@gmail.com>
2024-04-21 23:49:08 +0530

    logout handler

--- components/Navbar/ProfileIcon.jsx
+++ components/Navbar/ProfileIcon.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { CircleUser, Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
@@ -6,10 +7,39 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
-import { useSelector } from "react-redux";
+import { Loader } from "lucide-react";
+import { useSelector, useDispatch } from "react-redux";
+import { useToast } from "@/components/ui/use-toast";
+import { redirect } from "next/navigation";
+
+import authService from "@/appwrite/auth";
+import { logout } from "@/store/AuthSlice";
 
 export function ProfileIcon() {
+  const [loading, setLoading] = useState(false);
   const userData = useSelector((state) => state.auth.userData);
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+
+  const handleLogout = async () => {
+    setLoading(true);
+    const logoutSuccess = await authService.logout();
+    if (logoutSuccess) {
+      toast({
+        title: "Logout Success",
+        description: "Logged out",
+      });
+      dispatch(logout());
+      redirect("/login");
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Logout Failed",
+        description: "Failed to logout",
+      });
+    }
+    setLoading(false);
+  };
   return (
     <div className="cursor-pointer">
       <HoverCard>
@@ -29,7 +59,14 @@ export function ProfileIcon() {
               </div>
             </div>
             <div className="flex items-end">
-              <Button variant="secondary">Logout</Button>
+              <Button
+                disabled={loading}
+                variant="secondary"
+                onClick={handleLogout}
+              >
+                {loading && <Loader className="w-4 h-4 mr-2 animate-spin" />}
+                Logout
+              </Button>
             </div>
           </div>
         </HoverCardContent>
